abfb6a42122be3185d3725e6d6e3bbab8ab9763d
Tim Williams <timwilliams.txW@gmail.com>
2019-01-15 20:50:05 +0000

    add random string generator

--- express_server.js
+++ express_server.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//found solution on stackoverflow
+//setting Math.random toString with 36 as a parameter sets number >9 to alpha characters
+function generateRandomString() {
+  let shortUrl = Math.random().toString(36).substring(2, 8);
+  console.log(shortUrl)
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -16,13 +25,24 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-console.log("hello")
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//return urls page to browser
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  generateRandomString(req)
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//add urls to template var
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
     urls: urlDatabase };
