119b3cd240cfa2e68e99cbd77d0dd4e6aa601570
Tim Williams <timwilliams.tx@gmail.com>
2019-01-20 19:23:37 +0000

    add styles and lint code

--- express_server.js
+++ express_server.js
@@ -6,7 +6,6 @@ var cookieSession = require('cookie-session');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const bodyParser = require("body-parser");
-// const styles = require('./assets/styles.css')
 
 app.use(express.static('./assets'));
 
@@ -21,13 +20,13 @@ app.use(cookieSession({
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 
-}))
+}));
 
 //found solution to generate random string on stackoverflow
 //setting Math.random toString with 36 as a parameter sets number > 9 to alpha characters
 function generateRandomString() {
   let shortURL = Math.random().toString(36).substring(2, 8);
-  return shortURL
+  return shortURL;
 }
 
 const urlDatabase = {
@@ -54,13 +53,13 @@ const users = {
     email: "timwilliams.tx@gmail.com",
     password: "$2b$10$GqgM5tmIofmgL4DCcHAYD.JfmO3WVTcY3hmj9L9Q75kVpmPybramO"
   }
-}
+};
 
 //loop through url database
   //if user id = logged in user id
     //add to temp db
 let urlsForUser = id => {
-  let tempDb = {}
+  let tempDb = {};
   for (let key in urlDatabase) {
     if (urlDatabase[key].user_id === id) {
       tempDb[key] = {
@@ -69,33 +68,33 @@ let urlsForUser = id => {
         user_id: urlDatabase[key].user_id,
         date: urlDatabase[key].date,
         count: urlDatabase[key].count
-        }
+        };
       }
-    } return tempDb
-  }
+    } return tempDb;
+  };
 
 //counter to track number of times u/:id page is used
   let count = 0;
-  counter = function(id) {
+ 	let counter = function(id) {
     count = urlDatabase[id].count || 0;
     urlDatabase[id].count = count +=1;
     return count;
-    }
+    };
 
 //get date to use when url is created in /register
   var date = new Date();
-  var year = date.getFullYear()
+  var year = date.getFullYear();
   var month = date.getMonth() + 1;
       month = (month < 10 ? "0" : "") + month;
   var day  = date.getDate();
       day = (day < 10 ? "0" : "") + day;
-  var todaysDate = `${month}/${day}/${year}`
+  var todaysDate = `${month}/${day}/${year}`;
 
 // '/' returns 'Hello!'
 app.get("/", (req, res) => {
-  let userId = req.session.user_id
+  let userId = req.session.user_id;
   if (userId) {
-    res.redirect('/urls')
+    res.redirect('/urls');
   }
   res.redirect('/login');
 });
@@ -109,44 +108,42 @@ app.get("/urls.json", (req, res) => {
   //validate password is correct
     //if both pass set cookie and return to /
   // if fail, return 403
-  app.post("/login", (req, res) => {
-    for (let key in users) {
-      if (users[key].email === req.body.email) {
-        // if(users[key].password === req.body.password) {
-          let check;
-          let password = req.body.password;
-          check = bcrypt.compareSync(password, users[key].password)
-          if (check) {
-          req.session.user_id = users[key].id
-          res.redirect('/urls');
-        } res.status(403).send('Login or password is incorrect')
-      }
-    } res.status(403).send('Email address doesn\'t exist')
-  })
+app.post("/login", (req, res) => {
+  for (let key in users) {
+    if (users[key].email === req.body.email) {
+        let check;
+        let password = req.body.password;
+        check = bcrypt.compareSync(password, users[key].password);
+        if (check) {
+        req.session.user_id = users[key].id
+        res.redirect('/urls');
+      } res.status(403).send('<h1>Login or password is incorrect</h1>');
+    }
+  } res.status(403).send('<h1>Email address doesn\'t exist</h1>');
+});
 
 app.get('/login', (req, res) => {
   let user_id = req.session.user_id;
   if (user_id) {
-    res.redirect('/urls')
+    res.redirect('/urls');
   }
-  res.render('login')
-})
+  res.render('login');
+});
 
 //check if logged in, if not render page
 app.get('/register', (req, res) => {
   let user_id = req.session.user_id;
   if (user_id) {
-    res.redirect('/urls')
+    res.redirect('/urls');
   }
-  res.render('register')
-})
+  res.render('register');
+});
 
 //POST route for register
 //add new user to the user data
 app.post('/register', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
-  console.log(email, password)
   if (email) {
     if (password) {
       let inUsers = false;
@@ -162,7 +159,7 @@ app.post('/register', (req, res) => {
             id: newId,
             email: req.body.email,
             password: hashedPassword
-          }
+          };
             req.session.user_id = newId.id;
             res.redirect('/urls');
         } else {
@@ -183,27 +180,27 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let userId = req.session.user_id
+  let userId = req.session.user_id;
   if (userId) {
     let templateVars = { urls: urlsForUser(userId),
       user_id: users[req.session.user_id]
-    }
-    res.render("urls_index", templateVars);
+    };
+    res.render("urls_index", templateVars)
   } else {
-    res.send('Please login to access this page')
+    res.send('<h1>Please login to access this page</h1>');
   }
 });
 
 //return urls page to browser
 app.get("/urls/new", (req, res) => {
-  let userId = req.session.user_id
+  let userId = req.session.user_id;
   if (userId) {
     let templateVars = {
       user_id: users[req.session.user_id]
-    }
-    res.render('urls_new', templateVars)
+    };
+    res.render('urls_new', templateVars);
   } else {
-    res.redirect('/login')
+    res.redirect('/login');
   }
 });
 
@@ -216,21 +213,20 @@ app.post("/urls", (req, res) => {
     user_id: req.session.user_id,
     date: todaysDate,
     counter: 0
-  }
-  let newUrl = ('/urls/'+newShortUrl)
+  };
+  let newUrl = ('/urls/'+newShortUrl);
   res.redirect(newUrl);
 });
 
 
 app.get("/u/:shortURL", (req, res) => {
-  let urlId = req.params.shortURL
+  let urlId = req.params.shortURL;
   if (!urlDatabase[urlId]) {
-    console.log(urlDatabase[urlId]);
-    res.status(404).send('This URL does not exist');
+    res.status(404).send('<h1>This URL does not exist</h1>');
   } else {
-  let longURL = urlDatabase[urlId].url
+  let longURL = urlDatabase[urlId].url;
   res.redirect(longURL);
-   counter(urlId)
+   counter(urlId);
   }
 });
 
@@ -243,7 +239,7 @@ app.get("/u/:shortURL", (req, res) => {
 //else return page
 app.get("/urls/:id", (req, res) => {
   let urlId = req.params.id;
-  let user_id = req.session.user_id
+  let user_id = req.session.user_id;
 
   if (user_id) {
     if (urlDatabase[urlId]) {
@@ -253,13 +249,13 @@ app.get("/urls/:id", (req, res) => {
           user_id: users[req.session.user_id] };
         res.render("urls_show", templateVars);
       } else {
-        res.send('Only URL owner can access this page')
+        res.send('<h1>Only URL owner can access this page</h1>');
       }
     } else {
-      res.status(404).send('This URL does not exist')
+      res.status(404).send('<h1>This URL does not exist</h1>');
     }
   } else {
-    res.send('Please login to access this page')
+    res.send('<h1>Please login to access this page</h1>');
   }
 });
 
@@ -267,36 +263,36 @@ app.get("/urls/:id", (req, res) => {
 //if url owner is same as logged in user allow delete
 app.post('/urls/:id/delete', (req, res) => {
   let urlToDelete = req.params.id;
-  let user_id = req.session.user_id
+  let user_id = req.session.user_id;
 
   if (user_id) {
     if (urlDatabase[urlToDelete].user_id === user_id) {
-      delete urlDatabase[urlToDelete]
-      res.redirect('/urls')
+      delete urlDatabase[urlToDelete];
+      res.redirect('/urls');
     } else {
-      res.send('Only URL owner can delete')
+      res.send('<h1>Only URL owner can delete</h1>');
       }
     } else {
-    res.send('Please login to access this page')
+    res.send('<h1>Please login to access this page</h1>');
     }
 });
 
 //use POST to update url database entrty
 app.post('/urls/:id/update', (req, res) => {
   let urlToUpdateId = req.params.id;
-  let user_id = req.session.user_id
+  let user_id = req.session.user_id;
 
   if (user_id) {
     if (urlDatabase[urlToUpdateId].user_id === user_id) {
-      urlDatabase[urlToUpdateId].url = req.body.updatedUrl
-      res.redirect('/urls')
+      urlDatabase[urlToUpdateId].url = req.body.updatedUrl;
+      res.redirect('/urls');
     } else {
-      res.send('Only URL owner can delete')
+      res.send('<h1>Only URL owner can delete</h1>');
       }
     } else {
-    res.send('Please login to access this page')
+    res.send('<h1>Please login to access this page</h1>');
     }
-})
+});
 
 
 app.get("/hello", (req, res) => {
